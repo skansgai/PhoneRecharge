8cd128ed0dbd2fef433a7904e0383386499274eb
songsongyang <1026334022@qq.com>
2017-09-05 22:46:03 +0800

    规范了代码注释

--- app/src/main/java/huawei/com/activity/MainActivity.java
+++ app/src/main/java/huawei/com/activity/MainActivity.java
@@ -37,11 +37,9 @@ public class MainActivity extends Activity implements OnClickListener{
 				String resultInfo = payResult.getResult();// 同步返回需要验证的信息
 
 				String resultStatus = payResult.getResultStatus();
-				// 判断resultStatus 为“9000”则代表支付成功，具体状态码代表含义可参考接口文档
 				if (TextUtils.equals(resultStatus, "9000")) {
 					Toast.makeText(MainActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
 				} else {
-					// 判断resultStatus 为非"9000"则代表可能支付失败
 					// "8000"代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
 					if (TextUtils.equals(resultStatus, "8000")) {
 						Toast.makeText(MainActivity.this, "支付结果确认中", Toast.LENGTH_SHORT).show();
@@ -90,7 +88,6 @@ public class MainActivity extends Activity implements OnClickListener{
 			// 使用支付宝进行支付
 			new AliPayThread().start();
 			break;
-
 		default:
 			break;
 		}
